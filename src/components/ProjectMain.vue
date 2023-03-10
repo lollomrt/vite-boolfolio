eed5e1839f6f24e271fd3b6fbe2951f3a3d4be3f
lollomrt <lorenzomartini.mrt@outlook.it>
2023-03-10 15:47:07 +0100

    warning in consolle

---
+++ src/components/ProjectMain.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    data() {
+        return {
+            projects: [],
+            loading: true,
+            baseUrl: 'http://127.0.0.1:8000'
+        }
+    },
+    methods: {
+        getProjects() {
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/project`).then((response) => {
+                if (response.data.success) {
+                    this.projects = response.data.results
+                    this.loading = false;
+                }
+                else {
+                    //messaggio di errore
+                }
+            })
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Lista dei progetti</h1>
+                </div>
+                <div class="col-12">
+                    lista
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/pd.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div>
+        pd
+    </div>
+</template>
+<style lang="">
+    
+</style>
