96d8114cb4d8ccceac9a58a8079334be3679a535
lollomrt <lorenzomartini.mrt@outlook.it>
2023-03-10 20:38:35 +0100

    milestone 5 completa

--- src/components/ProjectMain.vue
+++ src/components/ProjectMain.vue
@@ -1,6 +1,7 @@
 <script>
 
 import axios from 'axios';
+import SingleProject from './SingleCard.vue';
 import { store } from '../store.js'
 
 export default {
@@ -8,6 +9,9 @@ export default {
         return {
             store
         }
+    },
+    components: {
+        SingleProject
     },
     methods: {
         getProjects() {
@@ -48,19 +52,7 @@ export default {
                     </div>
                     <div class="col-12">
                         <div class="container g-0 cards-container mx-0 d-flex flex-wrap">
-                            <div class="card card-projects" v-for="project in store.projects" :key="project.id">
-                                <div class="card-image-top position-relative">
-                                    <img :src="project.cover_image != null ? `${store.baseUrl}/storage/${project.cover_image}` : 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'"
-                                        :alt="project.title">
-                                    <div class="position-absolute hover-img"></div>
-                                </div>
-                                <div class="card-body">
-                                    <div class="card-title">
-                                        <h5>{{ project.title }}</h5>
-                                        <p>{{ project.content }}</p>
-                                    </div>
-                                </div>
-                            </div>
+                            <SingleProject />
                         </div>
                     </div>
                 </div>
@@ -72,33 +64,6 @@ export default {
 <style lang="scss" scoped>
 .bg-fancy {
     background-color: #3f2b96;
-}
-
-.cards-container {
-
-    gap: 1rem;
-
-    .card-projects {
-        width: calc(100% / 4 - 1rem);
-        background-color: #8189d8;
-        border: none;
-        box-shadow: rgba(0, 0, 0, 0.18) 0px 2px 4px;
-
-        img {
-            width: 100%;
-            height: 200px;
-            object-fit: cover;
-        }
-
-        .hover-img {
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-            background-color: #8189d8;
-            mix-blend-mode: multiply;
-        }
-    }
 }
 
 .loader-container {
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,61 @@
+<script>
+
+import { store } from '../store.js'
+
+export default {
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="col-12">
+        <div class="container g-0 cards-container mx-0 d-flex flex-wrap">
+            <div class="card card-projects" v-for="project in store.projects" :key="project.id">
+                <div class="card-image-top position-relative">
+                    <img :src="project.cover_image != null ? `${store.baseUrl}/storage/${project.cover_image}` : 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'"
+                        :alt="project.title">
+                    <div class="position-absolute hover-img"></div>
+                </div>
+                <div class="card-body">
+                    <div class="card-title">
+                        <h5>{{ project.title }}</h5>
+                        <p>{{ project.content }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.cards-container {
+
+    gap: 1rem;
+
+    .card-projects {
+        width: calc(100% / 4 - 1rem);
+        background-color: #8189d8;
+        border: none;
+        box-shadow: rgba(0, 0, 0, 0.18) 0px 2px 4px;
+
+        img {
+            width: 100%;
+            height: 200px;
+            object-fit: cover;
+        }
+
+        .hover-img {
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            background-color: #8189d8;
+            mix-blend-mode: multiply;
+        }
+    }
+}
+</style>
